355d117abcdbeb517dae76f87280fb72407e0842
Fimosinha <150753537+argentinoboludo69@users.noreply.github.com>
2025-06-16 07:39:30 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'LULA',
+    'Em que ano Lula nasceu?',
+    'Lula nasceu em 1945, no dia 27 de outubro'
+)
+
+criaCartao(
+    'LULA',
+    'Quantas vezes Lula foi presidente do Brasil??',
+    'Lula foi presidente por dois mandatos: 2003-2007 e 2007-2011, e foi reeleito em 2022'
+)
+
+criaCartao(
+    'LULA',
+    'Qual partido político Lula ajudou a fundar??',
+    'Lula foi um dos fundadores do Partido dos Trabalhadores (PT) em 1980'
+)
+
+criaCartao(
+    'LULA',
+    'Qual foi a profissão de Lula antes da política??',
+    'Lula foi metalúrgico e líder sindical antes de se tornar político)'
+)
